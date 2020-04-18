8e0a2264ec54fb737039da404aa79ac7b6a0bd4a
jkojro <janusz@astarium.pl>
2020-04-18 08:36:09 +0200

    products count

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,8 @@ class Product < ApplicationRecord
   has_many :carts, through: :cart_products
 
   validates_presence_of :name, :price
+
+  def cart_products_count(cart_id)
+    cart_products.where(cart_id: cart_id).count
+  end
 end
